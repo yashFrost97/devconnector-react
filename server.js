fccc9c87105b2bc60ce9161835ea7790d9b9b339
Yash Patange <yash.frost1997@gmail.com>
2020-07-04 16:18:59 -0400

    React Frontend setup, Login and Register forms set up and can send requests using axios. Also setup proxy

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const app = express();
 connectDB();
 
 // middleware
-app.use(express.json({
-	extended: false
-}));
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 app.get("/", (req, res) => {
-	res.send("Running!");
+  res.send("Running!");
 });
 
 // DEFINE routes
@@ -20,9 +22,8 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-	console.log(`Server started on port: ${PORT}`);
+  console.log(`Server started on port: ${PORT}`);
 });
