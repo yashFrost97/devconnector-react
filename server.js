afbb90ecc08150f307711fadec2e79b622a71b52
Yash Patange <yash.frost1997@gmail.com>
2020-07-04 14:29:52 -0400

    Setup done!

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Server started on port: ${PORT}`);
