2d2e60292362acef2b1010588dfe6099278433cd
Yash Patange <yash.frost1997@gmail.com>
2020-07-16 12:29:01 -0400

    Section 42 updates

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,7 +1,28 @@
 import axios from "axios";
 import { setAlert } from "./alert";
 
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from "./types";
+import setAuthToken from "../utils/setAuthToken";
+
+// load user
+export const loadUser = () => async (dispatch) => {
+  // set token in global header
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get("/api/auth");
+
+    dispatch({ type: USER_LOADED, payload: res.data });
+  } catch (error) {
+    dispatch({ type: AUTH_ERROR });
+  }
+};
 
 // register user
 export const register = ({ name, email, password }) => async (dispatch) => {
@@ -19,9 +40,11 @@ export const register = ({ name, email, password }) => async (dispatch) => {
     dispatch({ type: REGISTER_SUCCESS, payload: res.data });
   } catch (error) {
     const errors = error.response.data.errors;
+
     if (errors) {
       errors.forEach((err) => dispatch(setAlert(err.msg, "danger")));
     }
+
     dispatch({ type: REGISTER_FAIL });
   }
 };
